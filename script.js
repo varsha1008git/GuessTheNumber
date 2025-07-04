84bcc9d28685830ba79bb773d4a7ea14539f72e8
varsha1008git <varshaindar1008@gmail.com>
2025-07-04 09:26:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let snumber=Math.floor(Math.random()*100 + 1);
+let attempts=0;
+let maxattempts=10;
+function checkGuess(){
+    const guess=Number(document.getElementById("guessInput").value);
+    const message = document.getElementById("message");
+    const lives = document.getElementById("lives");
+    attempts++;
+    if(guess==snumber){
+         message.textContent = `🎉 Correct! You guessed it in ${attempts} attempts.`;
+        message.style.color="green";
+    }
+    else if(guess>snumber){
+        message.textContent="📈Your guess is too high!Try other number..";
+        message.style.color="orange";
+    }
+    else if(attempts>=maxattempts){
+         message.textContent = `💀 Game Over! The number was ${secretNumber}.`;
+        message.style.color = "red";
+    }
+    else {
+        message.textContent="📉Your guess is too low!!";
+         message.style.color="orange";
+    }
+    lives.textContent = `❤️ Lives remaining: ${maxAttempts - attempts}`;
+}
